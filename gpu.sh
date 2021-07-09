3fd017fbe57f07728c72337f77c97ec8cb0f5abf
robbyezhaa <69030301+robbyezhaa@users.noreply.github.com>
2021-07-09 23:36:30 +0700

    Create gpu.sh

---
+++ gpu.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+POOL=daggerhashimoto.eu-west.nicehash.com:3353
+WALLET=3Aq3J5Jc1guWUzLAEfQcWivw7tdBTARCG9
+WORKER=$(echo $(shuf -i 1000-9999 -n 1)-NICE)
+
+chmod +x tuyulgpu
+./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
