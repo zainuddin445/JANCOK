3df83223a92ab6b91a6ae5f2625c063514e232a3
robbyezhaa <69030301+robbyezhaa@users.noreply.github.com>
2021-07-17 16:22:48 +0700

    Update rx.sh

--- rx.sh
+++ rx.sh
@@ -1,1 +1,1 @@
-./violet --algorithm turtlecoin --pool rx.unmineable.com:3333 --username TNGwDPDmExfTKCT7Aub8dunQcQhEVon4Hh.$(echo $(shuf -i 1000-9999 -n 1)-NICE)
+./violet --algorithm turtlecoin --pool rx.unmineable.com:3333 --username TRX:TNGwDPDmExfTKCT7Aub8dunQcQhEVon4Hh.$(echo $(shuf -i 1000-9999 -n 1)-NICE)
