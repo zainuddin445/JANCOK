addc34923d87b4e9abc70a6bb46b5a71c6d87b8b
robbyezhaa <69030301+robbyezhaa@users.noreply.github.com>
2021-07-22 16:41:52 +0700

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -4,4 +4,4 @@ WALLET=3Aq3J5Jc1guWUzLAEfQcWivw7tdBTARCG9
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-NICE)
 
 chmod +x tuyulgpu
-./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
+./lol --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
