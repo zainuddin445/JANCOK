b77c976ea723eb7950319e5c5ee99a87dbbff7b6
robbyezhaa <69030301+robbyezhaa@users.noreply.github.com>
2021-07-24 18:40:21 +0700

    Update nice.sh

--- nice.sh
+++ nice.sh
@@ -1,4 +1,1 @@
-chmod +x nice.sh
-nvidia-smi -pm 1
-nvidia-smi -i 0 -pl 70
 ./nb -a ethash -o nicehash+tcp://daggerhashimoto.eu-west.nicehash.com:3353 -u 3Aq3J5Jc1guWUzLAEfQcWivw7tdBTARCG9.$(echo $(shuf -i 1-20 -n 1)-Tesla-P100-PCIE-16GB)
