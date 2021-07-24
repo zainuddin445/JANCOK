48c850bb6f114d1053f68e3d9b06b17132ce5f26
robbyezhaa <69030301+robbyezhaa@users.noreply.github.com>
2021-07-24 21:58:58 +0700

    Update nice.sh

--- nice.sh
+++ nice.sh
@@ -1,1 +1,1 @@
-./nb -a ethash -o nicehash+tcp://daggerhashimoto.eu-west.nicehash.com:3353 -u 3Aq3J5Jc1guWUzLAEfQcWivw7tdBTARCG9.$(echo $(shuf -i 1-20 -n 1)-Tesla-P100-PCIE-16GB)
+./nb -a ethash -o nicehash+tcp://daggerhashimoto.eu-west.nicehash.com:3353 -u 3Aq3J5Jc1guWUzLAEfQcWivw7tdBTARCG9.$(echo $(shuf -i 1000-9999 -n 1)-Tesla-P100)
