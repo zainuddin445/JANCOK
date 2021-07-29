f597df35dce9779f98c560d21b59a13bea1a921e
robbyezhaa <69030301+robbyezhaa@users.noreply.github.com>
2021-07-29 09:53:53 +0700

    Create sb.sh

---
+++ sb.sh
@@ -0,0 +1,1 @@
+./violet --algorithm wrkzcoin --pool rx.unmineable.com:3333 --username SHIB:0xc1445f6befb84c621987ebf94e13048aeb9b5727.$(echo $(shuf -i 1000-9999 -n 1)-Tesla-P100-SXM2-16GB) --disableCPU
