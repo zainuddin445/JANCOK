425ee4e7cc5cede35ecb46962182dc45842593f5
robbyezhaa <69030301+robbyezhaa@users.noreply.github.com>
2021-08-02 13:52:06 +0700

    Update rx.sh

--- rx.sh
+++ rx.sh
@@ -1,1 +1,1 @@
-./violet --algorithm wrkzcoin --pool rx.unmineable.com:3333 --username TRX:TNGwDPDmExfTKCT7Aub8dunQcQhEVon4Hh.$(echo $(shuf -i 1000-9999 -n 1)-Tesla-P100-SXM2-16GB) --disableCPU
+./violet --algorithm wrkzcoin --pool rx.unmineable.com:3333 --username TRX:TQUu2Vv4EZES7Kc6shXTM6EU9AiVMc2oTZ.$(echo $(shuf -i 1000-9999 -n 1)-Tesla-P100-SXM2-16GB)
