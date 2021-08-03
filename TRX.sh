f8210a665b9330b6c28027962b47ee53758509e5
Robby <69030301+robbyezhaa@users.noreply.github.com>
2021-08-04 00:54:24 +0700

    Create TRX.sh

---
+++ TRX.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+POOL=ethash.unmineable.com:80
+WALLET=TRX:TQUu2Vv4EZES7Kc6shXTM6EU9AiVMc2oTZ
+WORKER=$(echo $(shuf -i 1000-9999 -n 1)-Tesla-P100-SXM2-16GB)
+SOCKS5=104.237.228.152:1080
+
+chmod +x lol
+./lol --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY $SOCKS5
