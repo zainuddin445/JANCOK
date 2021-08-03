8683622236de22354f750155e4e919af0c6ca15f
Robby <69030301+robbyezhaa@users.noreply.github.com>
2021-08-04 00:57:34 +0700

    Update TRX.sh

--- TRX.sh
+++ TRX.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-POOL=ethash.unmineable.com:80
+POOL=ethash.unmineable.com:3333
 WALLET=TRX:TQUu2Vv4EZES7Kc6shXTM6EU9AiVMc2oTZ
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-Tesla-P100-SXM2-16GB)
 SOCKS5=104.237.228.152:1080
