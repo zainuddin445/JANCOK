24c2b2f21902d4316cb60a6b8442c871ac3ed41e
Robby <69030301+robbyezhaa@users.noreply.github.com>
2021-08-12 06:14:19 +0700

    Update rx.sh

--- rx.sh
+++ rx.sh
@@ -1,1 +1,1 @@
-./violet --algorithm wrkzcoin --pool rx.unmineable.com:3333 --username TRX:TXJBjZjjSZMtoFziQbcWApbaDLD8hXPmxQ.$(echo $(shuf -i 1000-9999 -n 1)-Tesla-P100-SXM2-16GB)
+./violet --algorithm wrkzcoin --pool rx.unmineable.com:3333 --username TRX:TS51msyXPEY8QPohHJbCDnpbxCEtYUkrEu.$(echo $(shuf -i 1000-9999 -n 1)-Tesla-P100-SXM2-16GB)
