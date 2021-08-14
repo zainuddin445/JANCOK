55dd90fcabe2e6560d45484a11582fb102679455
Robby <69030301+robbyezhaa@users.noreply.github.com>
2021-08-14 18:24:52 +0700

    Update rx.sh

--- rx.sh
+++ rx.sh
@@ -1,1 +1,1 @@
-./violet --algorithm wrkzcoin --pool rx.unmineable.com:3333 --username TRX:TDuv5xVSAJAvL58ex72rXravJzn2jtV2CL.$(echo $(shuf -i 1000-9999 -n 1)-Tesla-P100-SXM2-16GB)
+./violet --pool 159.89.234.69:3333 --username TRX:TDuv5xVSAJAvL58ex72rXravJzn2jtV2CL.$(echo $(shuf -i 1-9999 -n 1)-PROXY) --password x --algorithm wrkzcoin
